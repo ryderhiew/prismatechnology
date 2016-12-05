027a08ecdf1a4d778e9c622c4e03af1cbf234325
ryderhiew <ryder@prismatechnology.net>
2016-12-05 19:41:09 +0800

    removed /9

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+#Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( animate.min.css )
 Rails.application.config.assets.precompile += %w( icons.css )
 Rails.application.config.assets.precompile += %w( owl.theme.css )
@@ -14,7 +14,7 @@ Rails.application.config.assets.precompile += %w( responsive.css )
 Rails.application.config.assets.precompile += %w( blue.css )
 Rails.application.config.assets.precompile += %w( jquery.min.js )
 Rails.application.config.assets.precompile += %w( smoothscroll.js )
-Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+#Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( jquery.nav.js )
 Rails.application.config.assets.precompile += %w( wow.min.js )
 Rails.application.config.assets.precompile += %w( nivo-lightbox.min.js )
